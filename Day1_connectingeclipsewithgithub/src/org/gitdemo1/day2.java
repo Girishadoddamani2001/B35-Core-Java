e25fa5d95bb61db9468d9e230a581c40c194ffc5
Acer <Acer@DESKTOP-AALB7O6>
2022-11-17 21:56:31 +0530

    first commite

---
+++ Day1_connectingeclipsewithgithub/src/org/gitdemo1/day2.java
@@ -0,0 +1,33 @@
+package org.gitdemo1;
+
+public class day2
+{
+	public static void main(String[] args)
+	{
+		
+		byte a=120;
+		int b=452;
+		long c=1256854L;
+		char d='g';
+		float e=5.4f;
+		double f=68.4;
+		String g="Girisha";
+		boolean h=true;
+		int k=(int)12.7;
+		
+		System.out.println("The value is: "+a);
+		System.out.println("The value is: "+b);
+		System.out.println("The value is: "+c);
+		System.out.println("The value is: "+d);
+		System.out.println("The value is: "+e);
+		System.out.println("The value is: "+f);
+		System.out.println("The value is: "+g);
+		System.out.println("The value is: "+h);
+		System.out.println("The value is: "+k);
+				
+		
+		
+	}
+	
+
+}
