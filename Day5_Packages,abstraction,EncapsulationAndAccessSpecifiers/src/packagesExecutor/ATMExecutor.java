85071eaf54bcbdc9ed19129b91f4a133bf67cad4
Acer <Acer@DESKTOP-AALB7O6>
2022-11-26 21:41:28 +0530

    first commit

---
+++ Day5_Packages,abstraction,EncapsulationAndAccessSpecifiers/src/packagesExecutor/ATMExecutor.java
@@ -0,0 +1,21 @@
+package packagesExecutor;
+
+import java.util.Scanner;
+
+import packagesdemo.ATM;
+
+//driver class
+public class ATMExecutor {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the card no: ");
+		String card_no=s.nextLine();
+		ATM obj=new ATM();
+		obj.display(card_no);
+		
+
+	}
+
+}
