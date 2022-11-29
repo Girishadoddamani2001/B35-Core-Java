c3d6437d642988bd10d23a1131bfe5a3fff7cfde
Acer <Acer@DESKTOP-AALB7O6>
2022-11-29 21:58:33 +0530

    first commit

---
+++ Day8_finalAndSuperKeyword/src/finalkeyworddemo/FinalClassExecutor.java
@@ -0,0 +1,12 @@
+package finalkeyworddemo;
+
+//driver class
+public class FinalClassExecutor {
+
+	public static void main(String[] args) {
+		FinalChildClass f=new FinalChildClass();
+		f.print();
+
+	}
+
+}
