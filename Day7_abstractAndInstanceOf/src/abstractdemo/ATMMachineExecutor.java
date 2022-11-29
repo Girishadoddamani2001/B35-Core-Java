249d830054e59e3417ea276575d540cba7dade8f
Acer <Acer@DESKTOP-AALB7O6>
2022-11-29 21:32:23 +0530

    first commit

---
+++ Day7_abstractAndInstanceOf/src/abstractdemo/ATMMachineExecutor.java
@@ -0,0 +1,14 @@
+package abstractdemo;
+
+public class ATMMachineExecutor {
+
+	public static void main(String[] args) {
+		ATMMachine a=new ATMMachineChild();
+		//ATMMachineChild a=new ATMMachineChild();
+		a.withdraw();
+		a.display();
+
+	}
+
+	}
+
