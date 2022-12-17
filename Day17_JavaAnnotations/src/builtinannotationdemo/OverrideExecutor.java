e4649821d770fdd976e01a26fc26cbdd4b503ec4
Acer <Acer@DESKTOP-AALB7O6>
2022-12-17 10:43:47 +0530

    first commit

---
+++ Day17_JavaAnnotations/src/builtinannotationdemo/OverrideExecutor.java
@@ -0,0 +1,13 @@
+package builtinannotationdemo;
+
+//driver class
+public class OverrideExecutor {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.display();
+		
+
+	}
+
+}
